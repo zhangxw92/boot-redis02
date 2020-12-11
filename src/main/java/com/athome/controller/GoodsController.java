c9f4d348100d89c2be6440ba118194867e0ba990
zhangxw03 <zhangxw_92@163.com>
2020-12-11 13:51:35 +0800

    redis-分布式锁分析

--- src/main/java/com/athome/controller/GoodsController.java
+++ src/main/java/com/athome/controller/GoodsController.java
@@ -6,6 +6,9 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @Author zhangxw03
  * @Dat 2020-12-09 14:32
@@ -18,10 +21,28 @@ public class GoodsController {
     private StringRedisTemplate stringRedisTemplate;
     @Value("${server.port}")
     private String serverPort;
+    private static final String REDIS_LOCK = "redisLock";
 
     @GetMapping("buy")
     public String buy() {
-        synchronized (this) {
+
+        //如果只设置了setnx，程序走到第36行，部署应用的程序down机了，
+        // 那么将无法释放rdis中存入的key，因此需要对这个key设置一个过期时间
+        //高并发程序一定要考虑原子性
+        //Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(REDIS_LOCK, UUID.randomUUID().toString());
+        //保证原子性
+        String value = UUID.randomUUID().toString();
+        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(REDIS_LOCK, value, 10, TimeUnit.SECONDS);
+//        if (flag) {
+//            stringRedisTemplate.expire(REDIS_LOCK, 10, TimeUnit.SECONDS);
+//
+//        }
+        if (!flag) {
+            return "抢锁失败";
+        }
+
+        try {
+            //synchronized (this) {
             String s = stringRedisTemplate.opsForValue().get("goods:001");
             int result = s == null ? 0 : Integer.parseInt(s);
             if (result > 0) {
@@ -32,6 +53,15 @@ public class GoodsController {
             } else {
                 System.out.println("商品已经售罄" + serverPort);
                 return "商品已经售罄";
+            }
+            // }
+        } catch (NumberFormatException e) {
+            System.out.println(e);
+            return "程序异常";
+        } finally {
+            //直接删除key，带来的问题是可能会错删其他线程的key;解决方法增加判断，如果value相同再删
+            if (stringRedisTemplate.opsForValue().get(REDIS_LOCK).equals(value)) {
+                stringRedisTemplate.delete(REDIS_LOCK);
             }
         }
 
